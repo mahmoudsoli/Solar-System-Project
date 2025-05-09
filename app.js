63b204a0bb77761999dc1f4132a407a56d5a6577
Mahmoud Soliman <mahmoudsoliman427@gmail.com>
2025-05-09 18:28:40 +0300

    update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect('mongodb+srv://supercluster.d8jj.mongodb.net/superData', {
     user: process.env.MONGO_USERNAME,
     pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
